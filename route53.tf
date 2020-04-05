d019fcd792e3d549b35419a7b69e913986030a9d
manoj8928 <manoj.bhagwat60@gmail.com>
2020-04-05 13:18:35 +0530

    Update route53.tf

---
+++ route53.tf
@@ -0,0 +1,6 @@
+resource "aws_route53_record" "bar1"{
+ zone_id = data.aws_route53_zone.zone.zone_id
+ name = "bar1.bar.example.org"
+ type = "CNAME"
+ ttl = "300"  
+ records = ["${aws_instance.bar1.public_dns}"]}
